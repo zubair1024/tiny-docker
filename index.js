631a65edc6939f97d36944935e22b6e3f87cdef2
Zubair Ahmed <zubair1024@gmail.com>
2019-09-13 23:42:41 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello world</h1>`);
+});
+
+app.listen(3001, () => {
+  console.log("Listening on 3001...");
+});
